2beda6b64fc71486738fbd0cdb94039e106185c7
Gaston Larriera <larriera.gaston@gmail.com>
2019-09-23 09:42:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoTask from './Components/TodoTask';
+import todosData from './Data/todosData';
+import { Spin } from 'antd';
+import "antd/dist/antd.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: todosData,
+      isLoading: true
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        todos: todosData,
+        isLoading: false
+      })
+    }, 1500)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+          if(todo.id === id){
+            todo.completed = !todo.completed;
+          } 
+          return todo;
+        });
+      return {todos: updatedTodos}
+    });
+  }
+
+  render(){
+    const todoTasks = this.state.todos.map(task => <TodoTask key={task.id} task={task} handleChange={this.handleChange}/>);
+
+    return (
+      <div className="App">
+        {this.state.isLoading ? <Spin /> : todoTasks}
+      </div>
+    );
+  }
 }
 
 export default App;
